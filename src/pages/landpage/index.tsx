b721abba57fce53d22cf20879c1bd7de94c7df1c
xsalles <xs.salles@gmail.com>
2025-03-23 21:48:03 -0300

    feat: added all sections of landing-page

--- src/pages/landpage/index.tsx
+++ src/pages/landpage/index.tsx
@@ -1,7 +1,70 @@
 import { HeroSection } from "../../template/HeroSection";
+import IllustrationRoadmap from "../../assets/landingPage/illustrations/study-ilustration 1.svg";
+import IllustrationChallenge from "../../assets/landingPage/illustrations/challenges-illustration.svg";
+import IllustrationNews from "../../assets/landingPage/illustrations/news-illustration.svg";
+import { SectionIllustration } from "../../template/SectionIllustration";
+
+const sections = [
+  {
+    layout: "left",
+    title: "NÃO SABE POR ONDE COMEÇAR?",
+    description:
+    (
+        <>
+        Não se limite, acesse nossos <span className="highlight">Roadmaps</span> com trilhas de aprendizado prontas para guiá-lo ao seu objetivo de ser um Dev.
+        </>
+    ),
+    image: IllustrationRoadmap,
+    textButton: "Roadmaps",
+    backgroundImage: "black",
+    colorButton: "white",
+  },
+  {
+    layout: "right",
+    title: "NÃO FIQUE DE FORA DAS NOVIDADES DO MUNDO DEV!",
+    description:
+    (
+        <>
+        Acesse a nossa aba de <span className="highlight">Notícias</span> para ficar por dentro das notícias do mundo dev, além de ficar atualizado sobre as tecnologias do momento!
+        </>
+    ),
+    image: IllustrationNews,
+    textButton: "Notícias",
+    backgroundImage: "white",
+    colorButton: "black",
+  },
+  {
+    layout: "left",
+    title: "COLOQUE A MÃO NA MASSA E CRIE PROJETOS!",
+    description:
+    (
+        <>
+        Vá para a página de <span className="highlight">Desafios</span> e se aventure nos desafios que a plataforma te disponibiliza.
+        </>
+    ),
+    image: IllustrationChallenge,
+    textButton: "Desafios",
+    backgroundImage: "black",
+    colorButton: "white",
+  },
+];
 
 export function Landpage() {
-    return (
-        <HeroSection />
-    )
+  return (
+    <>
+      <HeroSection />
+      {sections.map((section, key) => (
+        <SectionIllustration
+          key={key}
+          layout={section.layout as "left" | "right"}
+          title={section.title}
+          description={section.description}
+          image={section.image}
+          textButton={section.textButton}
+          backgroundImage={section.backgroundImage as "black" | "white"}
+          colorButton={section.colorButton}
+        />
+      ))}
+    </>
+  );
 }
---
+++ src/template/SectionIllustration/styled.ts
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+import { theme } from "../../theme/theme";
+import blackBackground from "../../assets/backgrounds/black-background.svg"
+import whiteBackground from "../../assets/backgrounds/white-background.svg"
+
+interface SectionIllustrationProps {
+    layout: "left" | "right";
+    backgroundImage: "black" | "white";
+
+}
+
+export const SectionIllustrationContainer = styled.section<SectionIllustrationProps>`
+    display: flex;
+    flex-direction: ${({layout}) => layout === "right" ? "row-reverse" : "row"};
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    height: 100%;
+    padding: 262px 56px;
+    background-image: ${({backgroundImage}) => backgroundImage === "black" ? `url(${blackBackground})` : `url(${whiteBackground})`};
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+`
+
+export const SectionIllustrationContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    gap: 32px;
+    height: 100%;
+    width: 50%;
+`
+
+export const SectionIllustrationTitle = styled.h2<SectionIllustrationProps>`
+    font-family: 'Chakra Petch', sans-serif;
+    font-size: 72px;
+    font-weight: 700;
+    color: ${({backgroundImage}) => backgroundImage === "black" ? theme.colors.white : theme.colors.black};
+`
+
+export const SectionIllustrationDescription = styled.p<SectionIllustrationProps>`
+    font-size: 24px;
+    font-weight: 400;
+    font-style: italic;
+    color: ${({backgroundImage}) => backgroundImage === "black" ? theme.colors.white : theme.colors.black};
+    
+
+    .highlight {
+        color: ${theme.colors.blue400};
+        text-decoration: underline;
+    }
+`
+
+export const SectionIllustrationImage = styled.img`
+    width: 500px;
+    height: 500px;
+`
+
