23cd3c3eb32603a45376263a54db3b5ccbc462cf
ooliveiratg <menezesth16@gmail.com>
2025-04-06 22:56:04 -0300

    I added some grid-related components and brought a responsive layout

--- src/pages/RoadmapsPages/index.tsx
+++ src/pages/RoadmapsPages/index.tsx
@@ -1,11 +1,28 @@
+import { Button } from "../../components/Button";
+import { Grid } from "../../template/Roadmaps/GridButton/styled";
 import { HeroPageBackEnd } from "../../template/Roadmaps/Pages/HeroPages";
-import { SectionGroup } from "./styled";
+import { theme } from "../../theme/theme";
+import { Linguages } from "./Linguages";
+import { SectionGroup, SectionPage, TitleOfSection } from "./styled";
 
 export function RoadmapPages() {
-
-    return(
-        <SectionGroup>
-            <HeroPageBackEnd/>
-        </SectionGroup>
-    )
+  return (
+    <>
+      <SectionGroup>
+        <HeroPageBackEnd />
+      </SectionGroup>
+      <SectionPage>
+            <TitleOfSection>
+            Linguagens de programação
+            </TitleOfSection>
+            <Grid>
+                {
+                    Linguages.map((Linguages,index)=>(
+                        <Button key={index} width={"369px"} height={"81px"} colorText={`${theme.colors.white}`} bgColor={"transparent"} fontWeight={"600"} text={Linguages} border={`1px solid ${theme.colors.white}`}/>
+                    ))
+                }
+            </Grid>
+        </SectionPage>
+    </>
+  );
 }
--- src/template/Roadmaps/Pages/HeroPages/styled.ts
+++ src/template/Roadmaps/Pages/HeroPages/styled.ts
@@ -12,6 +12,12 @@ export const ImageContainer = styled.div`
 export const ImageIllustration = styled.img`
   width: 365px;
   height: 645px;
+
+  @media (max-width:375px) {
+    width: 330px;
+    height: 645px;
+    
+  }
 `
 
 export const InformationsContainer = styled.div`
@@ -22,8 +28,8 @@ export const InformationsContainer = styled.div`
   color: ${theme.colors.white};
   font-weight: 600;
   gap: 96px;
-  padding-bottom:137px;
   padding-left: 32px
+  
 `
 
 export const TitleHeroPage = styled.h2`
@@ -31,14 +37,49 @@ export const TitleHeroPage = styled.h2`
   font-size: 120px;
   padding-top:32px ;
   width: 332px;
+
+  @media (max-width:884px) {
+    width: 647px;
+    padding-top:24px ;
+    }
+
+    @media (max-width:428px) {
+      font-size: 60px;
+      padding-top: 32px;
+      width: 332px;
+      
+    }
 `
 export const SubTitleHeroPage = styled.h3`
     font-size: 32px;
     font-style: italic;
+
+    @media (max-width:884px) {
+    font-size:40px ;
+    }
+
+    @media (max-width:428px) {
+      font-size: 32px;
+    }
 `
 
 export const InformationsBackEnd = styled.p`
     text-align: justify;
     width: 577px;
     font-size: 24px;
+    padding-bottom:137px;
+
+    @media (max-width:884px) {
+    font-size:36px ;
+    padding-bottom: 24px ;
+    width: 710px;
+    
+    }
+
+    @media (max-width:428px) {
+      font-size: 24px;
+      padding-bottom: 33px;
+      width: 332px;
+      
+    }
 `
